0e9927a7e45a2e4896b07f35ab0058daf2d6e2da
hiram.001 <hiram.001@f222a810-1cc0-aae4-d1d1-676cc2844f09>
2010-06-27 02:08:01 +0000

    zombieKiosk changelog 06272010
    *Added config file feature
    *<security bug>AboutBox URL's open on GTK default browser [allowing to launch another programs] FIXED
    *Minor changes and code cleanup
    
    wøRg <worg@linuxmail.org>

--- trunk/zKiosk.py
+++ trunk/zKiosk.py
@@ -1,29 +1,43 @@
+#/usr/bin/env python
 import gtk
 import pygtk
 import webkit
+import ConfigParser
+from os import popen
+
+#Introducimos el valor por default a la configuracion, en caso de que no exista
+cfg = ConfigParser.SafeConfigParser({'web': 'http://148.204.48.96/uhtbin/webcat', 'theme': 'gtkrc'})
+# Leemos el archivo de configuracion
+cfg.read(["config.cfg"])  
+#Asignamos los valores de la configuracion a variables para su uso posterior
+web = cfg.get("Biblio","web")
+theme = cfg.get("Biblio","theme")
+#Parseamos el archivo del estilo visual
+gtk.rc_parse(theme)
+
 class Window:
 	def __init__(self):
-		self.builder = gtk.Builder()
+		self.builder = gtk.Builder() 
 		self.builder.add_from_file('zkiosk-ui.glade')
 		self.window = self.builder.get_object('window1')
 		self.Browser = self.builder.get_object('Browser')
-		self.window.show_all()
 		
-		self.webview = webkit.WebView()
+		#inicializa el widget del motor de renderizado y lo agrega a la interfaz
+		self.webview = webkit.WebView() 
 		self.Browser.add(self.webview)
-		self.Browser.show_all()
 		
 		#cambiando a pantalla completa
 		maxx = gtk.gdk.screen_width() 
 		maxy = gtk.gdk.screen_height() 
 		self.window.set_size_request(maxx,maxy) 
 		
-		#conectando los botones a las funciones
-		self.builder.connect_signals(self)
+		self.window.show_all() #mustra los elementos de la ventana
 		
-            
+		#conectando los botones y eventos de la ventana a las funciones 
+		self.builder.connect_signals(self)
+		            
 	def home(self, widget):
-		self.webview.open("http://148.204.48.96/uhtbin/webcat")
+		self.webview.open(web)
 				
 	def back(self, widget):
 		self.webview.go_back()
@@ -35,17 +49,25 @@ class Window:
 		self.webview.reload()
 		
 	def about(self, widget):
-		self.window.set_modal(False)
-		self.About=self.builder.get_object('aboutb')
-		Respuesta = self.About.run()
-		if Respuesta == gtk.RESPONSE_DELETE_EVENT or Respuesta == gtk.RESPONSE_CANCEL:
+		self.window.set_modal(False) #Quita la exclusividad del foco de la ventana principal y permite controlar el cuadro de acerca de..
+		self.About=self.builder.get_object('aboutb') #Accesamos al objeto correspondiente a ese dialogo
+		
+		def openW(widget,url,url2): # Evita abrir el sitio en el cuadro de dialogo acerca de
+			print url
+		
+		gtk.about_dialog_set_url_hook(openW,"") # Evita abrir el sitio en el cuadro de dialogo acerca de
+		# Obtenemos los eventos generados
+		Response = self.About.run()
+		#Si se presiona el boton de cerrar o se cierra el cuadro lo oculta y restaura el foco en la ventana principal
+		if Response == gtk.RESPONSE_DELETE_EVENT or Response == gtk.RESPONSE_CANCEL: 
 			self.About.hide()
 			self.window.set_modal(True)
-	
-	def noclose(widget, event,data):
+							
+	def noclose(widget, event,data): #evita que se cierre la ventana principal
 		return True
 				
 if __name__ == '__main__':
 	w = Window()
-	w.webview.open("http://148.204.48.96/uhtbin/webcat")
+	popen("xsetroot -cursor_name left_ptr")
+	w.webview.open(web)
 	gtk.main()
