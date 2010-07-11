2481962f43eb20f0cc996a0fec5632d652488f7e
hiram.001 <hiram.001@f222a810-1cc0-aae4-d1d1-676cc2844f09>
2010-07-11 18:37:15 +0000

    zombieKiosk changelog 07112010
    *Solved a Really STUPID bug [wrong shebang]
    *Using WebView.load_uri() instead of deprecated WebView.open() 
    *Config File is read from an absolute path
    *New user-agent (zombieKiosk/TequilaEngine)
    *Version updated to 1.1.2
    
    wøRg <worg@linuxmail.org>

--- trunk/zKiosk.py
+++ trunk/zKiosk.py
@@ -1,35 +1,45 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 import gtk
 import pygtk
 import webkit
 import ConfigParser
 from os import popen
+from commands import getoutput
+import psyco
+
+psyco.full()
 
 #Introducimos el valor por default a la configuracion, en caso de que no exista
-cfg = ConfigParser.SafeConfigParser({'web': 'http://148.204.48.96/uhtbin/webcat', 'theme': 'gtkrc'})
+cfg = ConfigParser.SafeConfigParser({"web": "http://148.204.48.96/uhtbin/webcat", "theme": "gtkrc"})
 # Leemos el archivo de configuracion
-cfg.read(["config.cfg"])  
+localpath = getoutput("pwd") + '/'
+cfg.read([localpath +"config.cfg"])  
 #Asignamos los valores de la configuracion a variables para su uso posterior
 web = cfg.get("Biblio","web")
 theme = cfg.get("Biblio","theme")
-#Parseamos el archivo del estilo visual
-gtk.rc_parse(theme)
-
+	
 class Window:
 	def __init__(self):
 		self.builder = gtk.Builder() 
 		self.builder.add_from_file('zkiosk-ui.glade')
 		self.window = self.builder.get_object('window1')
 		self.Browser = self.builder.get_object('Browser')
-		
+
 		#inicializa el widget del motor de renderizado y lo agrega a la interfaz
 		self.webview = webkit.WebView() 
 		self.Browser.add(self.webview)
-		
+		Settings = self.webview.get_settings()
+		Settings.set_property("user-agent","Mozilla/5.0 (X11; U; Linux i686; es-mx) AppleWebKit/531.2+ (KHTML, like Gecko) zombieKiosk/TequilaEngine 0.1.2 Safari/531.2+")
 		#cambiando a pantalla completa
 		maxx = gtk.gdk.screen_width() 
 		maxy = gtk.gdk.screen_height() 
 		self.window.set_size_request(maxx,maxy) 
+		
+		#Parseamos el archivo del estilo visual
+		gtk.rc_reset_styles(self.window.get_settings())
+		gtk.rc_parse(theme)
+		gtk.rc_add_default_file(theme)
+		gtk.rc_reparse_all()
 		
 		self.window.show_all() #mustra los elementos de la ventana
 		
@@ -37,7 +47,7 @@ class Window:
 		self.builder.connect_signals(self)
 		            
 	def home(self, widget):
-		self.webview.open(web)
+		self.webview.load_uri(web)
 				
 	def back(self, widget):
 		self.webview.go_back()
@@ -69,5 +79,5 @@ class Window:
 if __name__ == '__main__':
 	w = Window()
 	popen("xsetroot -cursor_name left_ptr")
-	w.webview.open(web)
+	w.webview.load_uri(web)
 	gtk.main()
